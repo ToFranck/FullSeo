bc628e8fb15561ad678875b0c198a78bb86859bb
Franck Tolosano <tolosanofranck@hotmail.com>
2022-10-31 17:22:24 +0100

    js

---
+++ script.js
@@ -0,0 +1,10 @@
+const observer = lozad();
+observer.observe();
+lozad('.lozad', {
+    load: function(el) {
+        el.src = el.dataset.src;
+        el.onload = function() {
+            el.classList.add('fade')
+        }
+    }
+}).observe()
